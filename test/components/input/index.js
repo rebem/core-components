a3ffa97f0f17bb8222357c63148d009104f89f5f
deepsweet <kir@soulshine.in>
2015-12-03 15:35:09 +0700

    :space_invader: Input tests

--- test/components/input/index.js
+++ test/components/input/index.js
@@ -1,5 +1,7 @@
+import Yummies from '@yummies/yummies';
+import YummiesDOM from '@yummies/dom';
 import TestUtils from 'react-addons-test-utils';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { render } from 'test/helpers/render';
 
 import Input from '#input';
@@ -12,6 +14,138 @@ describe('Input', () => {
 
         it('is a component', () => {
             expect(TestUtils.isCompositeComponent(render(Input()))).to.be.true;
+        });
+    });
+
+    describe('render', () => {
+        beforeEach(function() {
+            this.renderWithProps = props => {
+                this.rootComponent = render(Input(props));
+                this.inputControl = TestUtils.findRenderedDOMComponentWithTag(this.rootComponent, 'input');
+                this.rootComponentDOMNode = YummiesDOM.findDOMNode(this.rootComponent);
+                this.inputControlDOMNode = YummiesDOM.findDOMNode(this.inputControl);
+            };
+
+            this.renderWithProps();
+        });
+
+        describe('DOM', () => {
+            it('initial', function() {
+                expect(this.rootComponentDOMNode.className).to.be.a.block('input');
+                expect(this.inputControlDOMNode.className).to.be.an.elem({
+                    block: 'input',
+                    elem: 'control'
+                });
+            });
+
+            it('hover', function() {
+                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
+                expect(this.rootComponentDOMNode).to.have.mods({ hovered: true });
+
+                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
+                expect(this.rootComponentDOMNode).to.not.have.mods({ hovered: true });
+            });
+
+            it('focus/blur', function() {
+                TestUtils.Simulate.focus(this.inputControlDOMNode);
+                expect(this.rootComponentDOMNode).to.have.mods({ focused: true });
+
+                TestUtils.Simulate.blur(this.inputControlDOMNode);
+                expect(this.rootComponentDOMNode).to.not.have.mods({ focused: true });
+            });
+
+            it('disabled', function() {
+                this.renderWithProps({ disabled: true });
+                expect(this.rootComponentDOMNode).to.have.mods({ disabled: true });
+            });
+
+            it('value change', function() {
+                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: 'test' } });
+                expect(this.inputControlDOMNode.value).to.be.equal('test');
+            });
+
+            it('children', function() {
+                this.renderWithProps({
+                    children: Yummies.createElement('div', {
+                        key: 'test',
+                        className: 'test-children'
+                    })
+                });
+
+                expect(
+                    TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'test-children')
+                ).to.be.block('test-children');
+            });
+        });
+
+        describe('API', () => {
+            it('val()', function() {
+                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: 'test' } });
+                expect(this.rootComponent.val()).to.be.equal('test');
+            });
+        });
+
+        describe('callbacks', () => {
+            it('onChange', function() {
+                const spy = chai.spy();
+
+                this.renderWithProps({ onChange: e => {
+                    spy(e.target.value);
+                } });
+                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: 'cho' } });
+                expect(spy).to.have.been.called.with('cho');
+            });
+
+            it('onFocus', function() {
+                const spy = chai.spy();
+
+                this.renderWithProps({ onFocus: spy });
+                TestUtils.Simulate.focus(this.inputControlDOMNode);
+                expect(spy).to.have.been.called.once;
+            });
+
+            it('onBlur', function() {
+                const spy = chai.spy();
+
+                this.renderWithProps({ onBlur: spy });
+                TestUtils.Simulate.focus(this.inputControlDOMNode);
+                TestUtils.Simulate.blur(this.inputControlDOMNode);
+                expect(spy).to.have.been.called.once;
+            });
+
+            it('onMouseEnter', function() {
+                const spy = chai.spy();
+
+                this.renderWithProps({ onMouseEnter: spy });
+                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
+                expect(spy).to.have.been.called.once;
+            });
+
+            it('onMouseLeave', function() {
+                const spy = chai.spy();
+
+                this.renderWithProps({ onMouseLeave: spy });
+                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
+                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
+                expect(spy).to.have.been.called.once;
+            });
+        });
+
+        describe('componentWillReceiveProps', () => {
+            it('value', function() {
+                const div = document.createElement('div');
+                let Component = Yummies.render(Input({ value: 'test' }), div);
+
+                expect(Component.state.value).to.be.equal('test');
+
+                Component = Yummies.render(Input({ value: 'test2' }), div);
+
+                expect(Component.state.value).to.be.equal('test2');
+
+                Component = Yummies.render(Input({ value: 'test2' }), div);
+
+                expect(Component.state.value).to.be.equal('test2');
+            });
         });
     });
 });
--- test/index.js
+++ test/index.js
@@ -1,3 +1,10 @@
+import chai from 'chai';
+import chaiBEM from 'chai-bem';
+import chaiSpies from 'chai-spies';
+
+chai.use(chaiBEM());
+chai.use(chaiSpies);
+
 const componentsContext = require.context('../components/', true, /index\.js$/);
 const testsContext = require.context('./components/', true, /index\.js$/);
 

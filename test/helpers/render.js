6621f0e7aeaa57f835043a02b5b8694d5ecb1207
deepsweet <kir@soulshine.in>
2015-12-09 11:20:49 +0700

    :space_invader: add optional containerTag argument to render helpers

--- test/helpers/render.js
+++ test/helpers/render.js
@@ -1,13 +1,13 @@
 import YummiesDOM from '@yummies/dom';
 
-export function createRender() {
-    const dummy = document.createElement('div');
+export function createRender(containerTag = 'div') {
+    const dummy = document.createElement(containerTag);
 
     return function(instance) {
         return YummiesDOM.render(instance, dummy);
     };
 }
 
-export function renderOnce(instance) {
-    return createRender()(instance);
+export function renderOnce(instance, containerTag) {
+    return createRender(containerTag)(instance);
 }

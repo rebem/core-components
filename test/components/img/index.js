212aaa3cc48e4cb2d5a21daf3bd2b365284938bc
deepsweet <kir@soulshine.in>
2015-12-09 11:01:33 +0700

    :space_invader: Img tests

--- test/components/img/index.js
+++ test/components/img/index.js
@@ -1,4 +1,5 @@
 import TestUtils from 'react-addons-test-utils';
+import YummiesDOM from '@yummies/dom';
 import { expect } from 'chai';
 import { renderOnce } from 'test/helpers/render';
 
@@ -12,6 +13,16 @@ describe('img', () => {
 
         it('is a component', () => {
             expect(TestUtils.isCompositeComponent(renderOnce(Img()))).to.be.true;
+        });
+    });
+
+    describe('DOM', () => {
+        it('initial', function() {
+            const rootComponent = renderOnce(Img());
+            const rootComponentDOMNode = YummiesDOM.findDOMNode(rootComponent);
+
+            expect(rootComponentDOMNode).to.be.a.block('img');
+            expect(rootComponentDOMNode.tagName).to.be.equal('IMG');
         });
     });
 });

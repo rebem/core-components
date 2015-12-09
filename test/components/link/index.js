99fd0c5065974ba6aec443ed2df96407221e12f5
deepsweet <kir@soulshine.in>
2015-12-09 11:06:46 +0700

    :space_invader: Link tests

--- test/components/link/index.js
+++ test/components/link/index.js
@@ -1,3 +1,5 @@
+import Yummies from '@yummies/yummies';
+import YummiesDOM from '@yummies/dom';
 import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
 import { renderOnce } from 'test/helpers/render';
@@ -12,6 +14,43 @@ describe('link', () => {
 
         it('is a component', () => {
             expect(TestUtils.isCompositeComponent(renderOnce(Link()))).to.be.true;
+        });
+    });
+
+    describe('DOM', () => {
+        beforeEach(function() {
+            this.renderWithProps = props => {
+                this.rootComponent = renderOnce(Link(props));
+                this.rootComponentDOMNode = YummiesDOM.findDOMNode(this.rootComponent);
+            };
+
+            this.renderWithProps();
+        });
+
+        it('initial', function() {
+            expect(this.rootComponentDOMNode).to.be.a.block('link');
+            expect(this.rootComponentDOMNode.tagName).to.be.equal('A');
+        });
+
+        it('props', function() {
+            this.renderWithProps({
+                href: '#test'
+            });
+
+            expect(this.rootComponentDOMNode.href).to.be.equal(location.href + '#test');
+        });
+
+        it('children', function() {
+            this.renderWithProps({
+                children: Yummies.createElement('div', {
+                    key: 'test',
+                    className: 'test-children'
+                })
+            });
+
+            expect(
+                TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'test-children')
+            ).to.be.block('test-children');
         });
     });
 });

a44fae06d9730c776be22c6acdec8b1b48b79a6e
deepsweet <kir@soulshine.in>
2015-12-14 11:21:06 +0700

    :space_invader: more LabelGroup tests

--- test/components/label-group/index.js
+++ test/components/label-group/index.js
@@ -1,4 +1,6 @@
 import TestUtils from 'react-addons-test-utils';
+import Yummies from '@yummies/yummies';
+import YummiesDOM from '@yummies/dom';
 import { expect } from 'chai';
 import { renderOnce } from 'test/helpers/render';
 
@@ -12,6 +14,76 @@ describe('labelGroup', () => {
 
         it('is a component', () => {
             expect(TestUtils.isCompositeComponent(renderOnce(LabelGroup()))).to.be.true;
+        });
+    });
+
+    describe('DOM', () => {
+        beforeEach(function() {
+            this.renderWithProps = props => {
+                this.rootComponent = renderOnce(LabelGroup(props));
+                this.rootComponentDOMNode = YummiesDOM.findDOMNode(this.rootComponent);
+            };
+        });
+
+        it('initial', function() {
+            this.renderWithProps({
+                labelText: 'test label',
+                children: Yummies.createElement('div', {
+                    key: 'test',
+                    className: 'test-children'
+                })
+            });
+
+            const labelDOMNode = this.rootComponentDOMNode.children[0];
+            const controlDOMNode = this.rootComponentDOMNode.children[1];
+
+            expect(this.rootComponentDOMNode.tagName).to.be.equal('LABEL');
+            expect(this.rootComponentDOMNode).to.be.a.block('label-group');
+            expect(this.rootComponentDOMNode).to.has.mods({
+                'control-position': 'right'
+            });
+
+            expect(labelDOMNode).to.be.an.elem({
+                block: 'label-group',
+                elem: 'label'
+            });
+
+            expect(labelDOMNode.textContent).to.be.equal('test label');
+
+            expect(controlDOMNode).to.be.an.elem({
+                block: 'label-group',
+                elem: 'control'
+            });
+
+            expect(controlDOMNode.children[0]).to.be.block('test-children');
+        });
+
+        it('control-position_left', function() {
+            this.renderWithProps({
+                labelText: 'test label',
+                controlPosition: 'left',
+                children: Yummies.createElement('div', {
+                    key: 'test',
+                    className: 'test-children'
+                })
+            });
+
+            const controlDOMNode = this.rootComponentDOMNode.children[0];
+            const labelDOMNode = this.rootComponentDOMNode.children[1];
+
+            expect(controlDOMNode).to.be.an.elem({
+                block: 'label-group',
+                elem: 'control'
+            });
+
+            expect(controlDOMNode.children[0]).to.be.block('test-children');
+
+            expect(labelDOMNode).to.be.an.elem({
+                block: 'label-group',
+                elem: 'label'
+            });
+
+            expect(labelDOMNode.textContent).to.be.equal('test label');
         });
     });
 });

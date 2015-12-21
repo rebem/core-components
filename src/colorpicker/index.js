08c62dd285c8c738e007f7b9057d250264bc619f
mistadikay <koltsov.d@gmail.com>
2015-12-21 15:46:22 +0700

    :wrench: refactor colorpicker without cross-component inheritance

--- src/colorpicker/index.js
+++ src/colorpicker/index.js
@@ -1,18 +1,106 @@
-import InputClass from '#input?raw&-styles';
-
-export default Base => class extends InputClass(Base) {
+export default Base => class extends Base {
     static displayName = 'core: colorpicker';
 
-    render() {
-        const template = super.render();
+    constructor(props) {
+        super(props);
 
-        template.block = 'colorpicker';
-
-        template.content[0].props = {
-            ...template.content[0].props,
-            type: 'color'
+        this.state = {
+            value: props.value || '',
+            focused: false,
+            hovered: false
         };
+    }
 
-        return template;
+    componentWillReceiveProps({ value }) {
+        if (this.props.value !== value) {
+            this.setState({
+                value
+            });
+        }
+    }
+
+    _onInputChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+
+        if (this.props.onChange) {
+            this.props.onChange(e);
+        }
+    }
+
+    _onInputFocus(e) {
+        this.setState({
+            focused: true
+        });
+
+        if (this.props.onFocus) {
+            this.props.onFocus(e);
+        }
+    }
+
+    _onInputBlur(e) {
+        this.setState({
+            focused: false
+        });
+
+        if (this.props.onBlur) {
+            this.props.onBlur(e);
+        }
+    }
+
+    _onInputMouseEnter(e) {
+        this.setState({
+            hovered: true
+        });
+
+        if (this.props.onMouseEnter) {
+            this.props.onMouseEnter(e);
+        }
+    }
+
+    _onInputMouseLeave(e) {
+        this.setState({
+            hovered: false
+        });
+
+        if (this.props.onMouseLeave) {
+            this.props.onMouseLeave(e);
+        }
+    }
+
+    val() {
+        return this.state.value;
+    }
+
+    render() {
+        return {
+            block: 'colorpicker',
+            tag: 'label',
+            mods: {
+                focused: this.state.focused,
+                hovered: this.state.hovered,
+                disabled: this.props.disabled || false
+            },
+            content: [
+                {
+                    elem: 'control',
+                    tag: 'input',
+                    props: {
+                        type: 'color',
+                        ...this.props,
+                        value: this.state.value,
+                        onChange: ::this._onInputChange,
+                        onFocus: ::this._onInputFocus,
+                        onBlur: ::this._onInputBlur,
+                        onMouseLeave: ::this._onInputMouseLeave,
+                        onMouseEnter: ::this._onInputMouseEnter,
+                        ref: 'control',
+                        key: 'control'
+                    }
+                },
+                ...[ this.props.children ]
+            ]
+        };
     }
 };

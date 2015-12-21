cd1fe6783a1b8cbdb4bc8a8f97a7e38719bc2b16
mistadikay <koltsov.d@gmail.com>
2015-12-21 15:54:38 +0700

    :wrench: refactor button without cross-component inheritance

--- src/button/index.js
+++ src/button/index.js
@@ -1,15 +1,74 @@
-import InputClass from '#input?raw&-styles';
-
-export default Base => class extends InputClass(Base) {
+export default Base => class extends Base {
     static displayName = 'core: button';
 
     constructor(props) {
         super(props);
 
         this.state = {
-            ...this.state,
+            value: props.value || '',
+            focused: false,
+            hovered: false,
             pressed: false
         };
+    }
+
+    componentWillReceiveProps({ value }) {
+        if (this.props.value !== value) {
+            this.setState({
+                value
+            });
+        }
+    }
+
+    _onInputChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+
+        if (this.props.onChange) {
+            this.props.onChange(e);
+        }
+    }
+
+    _onInputFocus(e) {
+        this.setState({
+            focused: true
+        });
+
+        if (this.props.onFocus) {
+            this.props.onFocus(e);
+        }
+    }
+
+    _onInputBlur(e) {
+        this.setState({
+            focused: false
+        });
+
+        if (this.props.onBlur) {
+            this.props.onBlur(e);
+        }
+    }
+
+    _onInputMouseEnter(e) {
+        this.setState({
+            hovered: true
+        });
+
+        if (this.props.onMouseEnter) {
+            this.props.onMouseEnter(e);
+        }
+    }
+
+    _onInputMouseLeave(e) {
+        this.setState({
+            hovered: false,
+            pressed: false
+        });
+
+        if (this.props.onMouseLeave) {
+            this.props.onMouseLeave(e);
+        }
     }
 
     _onInputMouseDown(e) {
@@ -32,33 +91,41 @@ export default Base => class extends InputClass(Base) {
         }
     }
 
-    _onInputMouseLeave(e) {
-        super._onInputMouseLeave(e);
-
-        this.setState({
-            pressed: false
-        });
+    val() {
+        return this.state.value;
     }
 
     render() {
-        const template = super.render();
-
-        template.block = 'button';
-        template.tag = 'span';
-
-        template.mods = {
-            ...template.mods,
-            pressed: this.state.pressed
+        return {
+            block: 'button',
+            tag: 'span',
+            mods: {
+                focused: this.state.focused,
+                hovered: this.state.hovered,
+                disabled: this.props.disabled || false,
+                pressed: this.state.pressed
+            },
+            content: [
+                {
+                    elem: 'control',
+                    tag: 'input',
+                    props: {
+                        type: 'button',
+                        ...this.props,
+                        value: this.state.value,
+                        onChange: ::this._onInputChange,
+                        onFocus: ::this._onInputFocus,
+                        onBlur: ::this._onInputBlur,
+                        onMouseLeave: ::this._onInputMouseLeave,
+                        onMouseEnter: ::this._onInputMouseEnter,
+                        onMouseDown: ::this._onInputMouseDown,
+                        onMouseUp: ::this._onInputMouseUp,
+                        ref: 'control',
+                        key: 'control'
+                    }
+                },
+                ...[ this.props.children ]
+            ]
         };
-
-        template.content[0].props = {
-            ...template.content[0].props,
-            type: 'button',
-            onMouseDown: ::this._onInputMouseDown,
-            onMouseUp: ::this._onInputMouseUp,
-            onMouseLeave: ::this._onInputMouseLeave
-        };
-
-        return template;
     }
 };

c5cf5eec3534dda74affcc996ce7b2a68145a648
deepsweet <kir@soulshine.in>
2015-12-21 15:50:23 +0700

    :wrench: refactor Attach without cross-component inheritance

--- src/attach/index.js
+++ src/attach/index.js
@@ -1,20 +1,52 @@
-import InputClass from '#input?raw&-styles';
-
-export default Base => class extends InputClass(Base) {
+export default Base => class extends Base {
     static displayName = 'core: attach';
+    static defaultProps = {
+        disabled: false
+    };
 
     constructor(props) {
         super(props);
 
         this.state = {
-            ...this.state,
             pressed: false,
+            focused: false,
+            hovered: false,
             value: null
         };
     }
 
     // file input doesn't allow to change it's value programmatically
     // componentWillReceiveProps() {}
+
+    _onInputChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+
+        if (this.props.onChange) {
+            this.props.onChange(e);
+        }
+    }
+
+    _onInputFocus(e) {
+        this.setState({
+            focused: true
+        });
+
+        if (this.props.onFocus) {
+            this.props.onFocus(e);
+        }
+    }
+
+    _onInputBlur(e) {
+        this.setState({
+            focused: false
+        });
+
+        if (this.props.onBlur) {
+            this.props.onBlur(e);
+        }
+    }
 
     _onInputMouseDown(e) {
         this.setState({
@@ -36,43 +68,82 @@ export default Base => class extends InputClass(Base) {
         }
     }
 
-    _onInputMouseLeave(e) {
-        super._onInputMouseLeave(e);
-
+    _onInputMouseEnter(e) {
         this.setState({
+            hovered: true
+        });
+
+        if (this.props.onMouseEnter) {
+            this.props.onMouseEnter(e);
+        }
+    }
+
+    _onInputMouseLeave(e) {
+        this.setState({
+            hovered: false,
             pressed: false
         });
+
+        if (this.props.onMouseLeave) {
+            this.props.onMouseLeave(e);
+        }
+    }
+
+    _renderValue() {
+        if (!('value' in this.props)) {
+            return null;
+        }
+
+        return {
+            elem: 'value',
+            tag: 'span',
+            props: {
+                key: 'value'
+            },
+            content: this.props.value
+        };
+    }
+
+    _renderChildren() {
+        return [].concat(this.props.children);
+    }
+
+    val() {
+        return this.state.value;
     }
 
     render() {
-        const template = super.render();
-
-        template.block = 'attach';
-
-        template.mods = {
-            ...template.mods,
-            pressed: this.state.pressed
-        };
-
-        template.content[0].props = {
-            ...template.content[0].props,
-            type: 'file',
-            onMouseDown: ::this._onInputMouseDown,
-            onMouseUp: ::this._onInputMouseUp,
-            onMouseLeave: ::this._onInputMouseLeave
-        };
-
-        if ('value' in this.props) {
-            template.content.push({
-                elem: 'value',
-                tag: 'span',
-                props: {
-                    key: 'value'
+        return {
+            block: 'attach',
+            tag: 'label',
+            mods: {
+                focused: this.state.focused,
+                hovered: this.state.hovered,
+                pressed: this.state.pressed,
+                disabled: this.props.disabled
+            },
+            content: [
+                {
+                    elem: 'control',
+                    tag: 'input',
+                    props: {
+                        type: 'file',
+                        ...this.props,
+                        value: this.state.value,
+                        onChange: ::this._onInputChange,
+                        onFocus: ::this._onInputFocus,
+                        onBlur: ::this._onInputBlur,
+                        onMouseDown: ::this._onInputMouseDown,
+                        onMouseUp: ::this._onInputMouseUp,
+                        onMouseLeave: ::this._onInputMouseLeave,
+                        onMouseEnter: ::this._onInputMouseEnter,
+                        ref: 'control',
+                        key: 'control'
+                    }
                 },
-                content: this.props.value
-            });
-        }
-
-        return template;
+                this._renderValue(),
+                ...this._renderChildren()
+            ]
+        };
     }
 };

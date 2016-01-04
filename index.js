3186bae6d93030dcc2a5a6db06f6131e5794be2d
deepsweet <kir@soulshine.in>
2016-01-04 14:26:36 +0700

    :heavy_check_mark: fix path resolving in index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var path = require('path');
 
 module.exports = {
     default: {
-        path: path.resolve('build/'),
+        path: path.resolve(__dirname, 'build/'),
         files: {
             main: 'index.js'
         }
     },
     src: {
-        path: path.resolve('src/'),
+        path: path.resolve(__dirname, 'src/'),
         files: {
             main: 'index.js'
         }

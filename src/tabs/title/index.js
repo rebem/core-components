19a734f7719fc5b15550d21e648bc933c91d6104
deepsweet <kir@soulshine.in>
2016-01-18 10:26:08 +0700

    :wrench: tabs/title as pure function

--- src/tabs/title/index.js
+++ src/tabs/title/index.js
@@ -1,48 +1,38 @@
-import { Component, PropTypes } from 'react';
+import { PropTypes } from 'react';
 import { BEM } from '@yummies/bem';
 
 const block = 'tabs';
 
-export default class extends Component {
-    static displayName = `core: ${block}/title`;
-    static propTypes = {
-        title: PropTypes.node.isRequired,
-        index: PropTypes.number.isRequired,
-        selected: PropTypes.number,
-        onTabChange: PropTypes.func
-    };
-
-    constructor(props, context) {
-        super(props, context);
-
-        this.selectTab = this.selectTab.bind(this);
-    }
-
-    selectTab() {
-        if ('onTabChange' in this.props) {
-            this.props.onTabChange(this.props.index);
-        }
-    }
-
-    render() {
-        return BEM(
-            {
-                ...this.props,
-                block,
-                elem: 'title',
-                onClick: this.selectTab,
-                mods: {
-                    selected: this.props.selected === this.props.index,
-                    ...this.props.mods
-                }
+export default function TabsTitle(props) {
+    return BEM(
+        {
+            props,
+            block,
+            elem: 'title',
+            mods: {
+                selected: props.selected === props.index,
+                ...props.mods
             },
-            BEM(
-                {
-                    block,
-                    elem: 'title-inner'
-                },
-                this.props.title
-            )
-        );
-    }
+            onClick() {
+                if ('onTabChange' in props) {
+                    props.onTabChange(props.index);
+                }
+            }
+        },
+        BEM(
+            {
+                block,
+                elem: 'title-inner'
+            },
+            props.title
+        )
+    );
 }
+
+TabsTitle.displayName = `core: ${block}/title`;
+TabsTitle.propTypes = {
+    title: PropTypes.node.isRequired,
+    index: PropTypes.number.isRequired,
+    selected: PropTypes.number,
+    onTabChange: PropTypes.func
+};

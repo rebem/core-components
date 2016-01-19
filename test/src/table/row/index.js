c60d9ce2b0ae6327cf5a251cd283223afb7d3648
deepsweet <kir@soulshine.in>
2016-01-19 15:00:29 +0700

    :space_invader: fix TableRow tests

--- test/src/table/row/index.js
+++ test/src/table/row/index.js
@@ -1,64 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
-
-import { renderOnce } from 'test/helpers/render';
+import { shallow } from 'enzyme';
 
 import TableRow from '#table/row';
 
-describe.skip('table/tr', () => {
-    describe('basic', () => {
-        it('exists', () => {
+describe('table/row', function() {
+    describe('basic', function() {
+        it('exists', function() {
             expect(TableRow).to.exist;
         });
 
-        it('is a component', () => {
-            expect(TestUtils.isCompositeComponent(renderOnce(TableRow(), 'tbody'))).to.be.true;
+        it('is a component', function() {
+            expect(TestUtils.isElement(TableRow())).to.be.true;
         });
     });
 
-    describe('render', () => {
-        beforeEach(function() {
-            this.renderWithProps = props => {
-                this.rootComponent = renderOnce(TableRow(props), 'tbody');
-                this.rootComponentDOMNode = ReactDOM.findDOMNode(this.rootComponent);
-            };
-
-            this.renderWithProps();
-        });
-
-        describe('DOM', () => {
-
+    describe('render', function() {
+        describe('DOM', function() {
             it('initial', function() {
-                expect(this.rootComponentDOMNode.tagName).to.be.equal('TR');
-                expect(this.rootComponentDOMNode).to.be.an.elem({
-                    block: 'table',
-                    elem: 'row'
-                });
-            });
+                const component = shallow(TableRow());
 
-            it('props', function() {
-                const testAttr = 'test';
-
-                this.renderWithProps({
-                    'data-test-attr': testAttr
-                });
-
-                expect(this.rootComponentDOMNode.getAttribute('data-test-attr')).to.be.equal(testAttr);
-            });
-
-            it('children', function() {
-                this.renderWithProps({
-                    children: React.createElement('td', {
-                        key: 'test',
-                        className: 'test-children'
-                    })
-                });
-
-                expect(
-                    TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'test-children')
-                ).to.be.block('test-children');
+                expect(component.type()).to.be.equal('tr');
+                expect(component).to.be.an.elem({ block: 'table', elem: 'row' });
             });
         });
     });

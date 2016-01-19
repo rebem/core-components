af1b08eeb3aba6a1f18a698f25eaa19550fd17dc
deepsweet <kir@soulshine.in>
2016-01-19 15:00:23 +0700

    :space_invader: fix TableFoot tests

---
+++ test/src/table/foot/index.js
@@ -0,0 +1,28 @@
+import TestUtils from 'react-addons-test-utils';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import TableFoot from '#table/foot';
+
+describe('table/foot', function() {
+    describe('basic', function() {
+        it('exists', function() {
+            expect(TableFoot).to.exist;
+        });
+
+        it('is a component', function() {
+            expect(TestUtils.isElement(TableFoot())).to.be.true;
+        });
+    });
+
+    describe('render', function() {
+        describe('DOM', function() {
+            it('initial', function() {
+                const component = shallow(TableFoot());
+
+                expect(component.type()).to.be.equal('tfoot');
+                expect(component).to.be.an.elem({ block: 'table', elem: 'foot' });
+            });
+        });
+    });
+});

2b98ca6ba5f507e9cf52b6016b98bf7d6680a137
deepsweet <kir@soulshine.in>
2016-01-19 12:30:46 +0700

    :wrench: Textarea as stateless function

--- test/src/textarea/index.js
+++ test/src/textarea/index.js
@@ -1,171 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createElement } from 'react';
 import TestUtils from 'react-addons-test-utils';
 import chai, { expect } from 'chai';
-
-import { createRender, renderOnce } from 'test/helpers/render';
+import { shallow } from 'enzyme';
 
 import Textarea from '#textarea';
 
-describe.skip('textarea', () => {
-    describe('basic', () => {
-        it('exists', () => {
+describe('textarea', function() {
+    describe('basic', function() {
+        it('exists', function() {
             expect(Textarea).to.exist;
         });
 
-        it('is a component', () => {
-            expect(TestUtils.isCompositeComponent(renderOnce(Textarea()))).to.be.true;
+        it('is a component', function() {
+            expect(TestUtils.isElement(Textarea())).to.be.true;
         });
     });
 
-    describe('render', () => {
+    describe('render', function() {
         beforeEach(function() {
-            this.renderWithProps = props => {
-                this.rootComponent = renderOnce(Textarea(props));
-                this.rootComponentDOMNode = ReactDOM.findDOMNode(this.rootComponent);
-                this.inputControlDOMNode = TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'textarea__control');
-            };
-
-            this.renderWithProps();
+            this.component = shallow(Textarea());
+            this.control = this.component.find('.textarea__control');
         });
 
-        describe('DOM', () => {
+        describe('DOM', function() {
             it('initial', function() {
-                expect(this.inputControlDOMNode.tagName).to.be.equal('TEXTAREA');
-                expect(this.rootComponentDOMNode).to.be.a.block('textarea');
-                expect(this.inputControlDOMNode).to.be.an.elem({
+                expect(this.control).to.have.tagName('textarea');
+                expect(this.component).to.be.a.block('textarea');
+                expect(this.control).to.be.an.elem({
                     block: 'textarea',
                     elem: 'control'
                 });
             });
 
-            it('hover', function() {
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.have.mods({ hovered: true });
-
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.not.have.mods({ hovered: true });
-            });
-
-            it('focus/blur', function() {
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.have.mods({ focused: true });
-
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.not.have.mods({ focused: true });
-            });
-
             it('disabled', function() {
-                this.renderWithProps({ disabled: true });
-                expect(this.rootComponentDOMNode).to.have.mods({ disabled: true });
-            });
-
-            it('value change', function() {
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: 'test' } });
-                expect(this.inputControlDOMNode.value).to.be.equal('test');
+                this.component.setProps({ disabled: true });
+                expect(this.component).to.have.mods({ disabled: true });
             });
 
             it('children', function() {
-                this.renderWithProps({
-                    children: React.createElement('div', {
+                this.component.setProps({
+                    children: createElement('div', {
                         key: 'test',
                         className: 'test-children'
                     })
                 });
 
-                expect(
-                    TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'test-children')
-                ).to.be.block('test-children');
-            });
-        });
-
-        describe('API', () => {
-            it('val()', function() {
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: 'test' } });
-                expect(this.rootComponent.val()).to.be.equal('test');
-            });
-        });
-
-        describe('callbacks', () => {
-            it('onChange', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({
-                    onChange(e) {
-                        spy(e.target.value);
-                    }
-                });
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: 'test' } });
-                expect(spy).to.have.been.called.with('test');
-
-                this.renderWithProps();
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: 'test' } });
-                expect(spy).to.have.been.called.with('test');
-            });
-
-            it('onFocus', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onFocus: spy });
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onBlur', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onBlur: spy });
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseEnter', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onMouseEnter: spy });
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseLeave', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onMouseLeave: spy });
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-        });
-
-        describe('componentWillReceiveProps', () => {
-            it('value', function() {
-                const render = createRender();
-                let component = render(Textarea({ value: 'test' }));
-
-                expect(component.state.value).to.be.equal('test');
-
-                component = render(Textarea({ value: 'test2' }));
-
-                expect(component.state.value).to.be.equal('test2');
-
-                component = render(Textarea({ value: 'test2' }));
-
-                expect(component.state.value).to.be.equal('test2');
+                expect(this.component.find('.test-children')).to.be.block('test-children');
             });
         });
     });

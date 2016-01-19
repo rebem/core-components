17b359f71e1e135381f6b031d067b12f224fac1f
deepsweet <kir@soulshine.in>
2016-01-19 16:33:29 +0700

    :space_invader: refactor Link tests with Enzyme

--- test/src/link/index.js
+++ test/src/link/index.js
@@ -1,57 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
-import { renderOnce } from 'test/helpers/render';
+import { shallow } from 'enzyme';
 
 import Link from '#link';
 
-describe.skip('link', () => {
-    describe('basic', () => {
-        it('exists', () => {
+describe('a', function() {
+    describe('basic', function() {
+        it('exists', function() {
             expect(Link).to.exist;
         });
 
-        it('is a component', () => {
-            expect(TestUtils.isCompositeComponent(renderOnce(Link()))).to.be.true;
+        it('is a component', function() {
+            expect(TestUtils.isElement(Link())).to.be.true;
         });
     });
 
-    describe('render', () => {
-        beforeEach(function() {
-            this.renderWithProps = props => {
-                this.rootComponent = renderOnce(Link(props));
-                this.rootComponentDOMNode = ReactDOM.findDOMNode(this.rootComponent);
-            };
-
-            this.renderWithProps();
-        });
-
-        describe('DOM', () => {
+    describe('render', function() {
+        describe('DOM', function() {
             it('initial', function() {
-                expect(this.rootComponentDOMNode.tagName).to.be.equal('A');
-                expect(this.rootComponentDOMNode).to.be.a.block('link');
-            });
+                const component = shallow(Link());
 
-            it('props', function() {
-                this.renderWithProps({
-                    href: '#test'
-                });
-
-                expect(this.rootComponentDOMNode.href).to.be.equal(location.href + '#test');
-            });
-
-            it('children', function() {
-                this.renderWithProps({
-                    children: React.createElement('div', {
-                        key: 'test',
-                        className: 'test-children'
-                    })
-                });
-
-                expect(
-                    TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'test-children')
-                ).to.be.block('test-children');
+                expect(component).to.have.tagName('a');
+                expect(component).to.be.a.block('link');
             });
         });
     });

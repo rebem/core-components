2965c15b3f43ae0cc17292aacddfeb278f434ca2
deepsweet <kir@soulshine.in>
2016-01-19 14:49:50 +0700

    :space_invader: Radio as stateless function

--- src/radio/index.js
+++ src/radio/index.js
@@ -1,151 +1,20 @@
-import { Component, PropTypes } from 'react';
 import { BEM } from 'rebem';
-import UID from 'component-uid';
-import EventEmitter from 'eventemitter3';
 
 const block = 'radio';
-const UID_LENGTH = 20;
-const radioGroup = new EventEmitter();
 
-export default class extends Component {
-    static displayName = `core: ${block}`;
-    static propTypes = {
-        checked: PropTypes.bool,
-        disabled: PropTypes.bool,
-        onChange: PropTypes.func,
-        onFocus: PropTypes.func,
-        onBlur: PropTypes.func,
-        onMouseEnter: PropTypes.func,
-        onMouseLeave: PropTypes.func
-    };
-    static defaultProps = {
-        checked: false
-    };
-
-    constructor(props, context) {
-        super(props, context);
-
-        this._uid = UID(UID_LENGTH);
-        this.state = {
-            ...this.state,
-            checked: props.checked
-        };
-
-        this._onInputChange = this._onInputChange.bind(this);
-        this._onInputFocus = this._onInputFocus.bind(this);
-        this._onInputBlur = this._onInputBlur.bind(this);
-        this._onInputMouseLeave = this._onInputMouseLeave.bind(this);
-        this._onInputMouseEnter = this._onInputMouseEnter.bind(this);
-        this._onRadioGroupChange = this._onRadioGroupChange.bind(this);
-    }
-
-    componentDidMount() {
-        radioGroup.on('change', this._onRadioGroupChange);
-    }
-
-    componentWillReceiveProps({ checked }) {
-        if (this.props.checked !== checked) {
-            this.setState({
-                checked
-            });
-        }
-    }
-
-    componentWillUnmount() {
-        radioGroup.off('change', this._onRadioGroupChange);
-    }
-
-    _onRadioGroupChange({ form, id }) {
-        if (this.refs.control.form === form && this._uid !== id) {
-            this.setState({
-                checked: false
-            });
-        }
-    }
-
-    _onInputChange(e) {
-        this.setState({
-            checked: e.target.checked
-        });
-
-        radioGroup.emit('change', {
-            form: this.refs.control.form,
-            id: this._uid
-        });
-
-        if (this.props.onChange) {
-            this.props.onChange(e);
-        }
-    }
-
-    _onInputFocus(e) {
-        this.setState({
-            focused: true
-        });
-
-        if (this.props.onFocus) {
-            this.props.onFocus(e);
-        }
-    }
-
-    _onInputBlur(e) {
-        this.setState({
-            focused: false
-        });
-
-        if (this.props.onBlur) {
-            this.props.onBlur(e);
-        }
-    }
-
-    _onInputMouseEnter(e) {
-        this.setState({
-            hovered: true
-        });
-
-        if (this.props.onMouseEnter) {
-            this.props.onMouseEnter(e);
-        }
-    }
-
-    _onInputMouseLeave(e) {
-        this.setState({
-            hovered: false
-        });
-
-        if (this.props.onMouseLeave) {
-            this.props.onMouseLeave(e);
-        }
-    }
-
-    render() {
-        return BEM(
-            {
-                block,
-                tag: 'label',
-                mods: {
-                    focused: this.state.focused,
-                    hovered: this.state.hovered,
-                    checked: this.state.checked,
-                    disabled: this.props.disabled,
-                    ...this.props.mods
-                }
-            },
-            BEM({
-                ...this.props,
-                block,
-                elem: 'control',
-                tag: 'input',
-                type: 'radio',
-                checked: this.state.checked,
-                onChange: this._onInputChange,
-                onFocus: this._onInputFocus,
-                onBlur: this._onInputBlur,
-                onMouseLeave: this._onInputMouseLeave,
-                onMouseEnter: this._onInputMouseEnter,
-                ref: 'control'
-            }),
-            this.props.children
-        );
-    }
+export default function Radio(props) {
+    return BEM(
+        {
+            block,
+            tag: 'label'
+        },
+        BEM({
+            ...props,
+            block,
+            elem: 'control',
+            tag: 'input',
+            type: 'radio'
+        }),
+        props.children
+    );
 }
--- test/src/radio/index.js
+++ test/src/radio/index.js
@@ -1,194 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createElement } from 'react';
 import TestUtils from 'react-addons-test-utils';
 import chai, { expect } from 'chai';
-
-import { createRender, renderOnce } from 'test/helpers/render';
+import { shallow } from 'enzyme';
 
 import Radio from '#radio';
 
-describe.skip('radio', () => {
-    describe('basic', () => {
-        it('exists', () => {
+describe('radio', function() {
+    describe('basic', function() {
+        it('exists', function() {
             expect(Radio).to.exist;
         });
 
-        it('is a component', () => {
-            expect(TestUtils.isCompositeComponent(renderOnce(Radio()))).to.be.true;
+        it('is a component', function() {
+            expect(TestUtils.isElement(Radio())).to.be.true;
         });
     });
 
-    describe('render', () => {
+    describe('render', function() {
         beforeEach(function() {
-            this.renderWithProps = props => {
-                this.rootComponent = renderOnce(Radio(props));
-                this.rootComponentDOMNode = ReactDOM.findDOMNode(this.rootComponent);
-                this.inputControlDOMNode = TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'radio__control');
-            };
-
-            this.renderWithProps();
+            this.component = shallow(Radio());
+            this.control = this.component.find('.radio__control');
         });
 
-        describe('DOM', () => {
+        describe('DOM', function() {
             it('initial', function() {
-                expect(this.inputControlDOMNode.tagName).to.be.equal('INPUT');
-                expect(this.inputControlDOMNode.type).to.be.equal('radio');
-                expect(this.rootComponentDOMNode).to.be.a.block('radio');
-                expect(this.inputControlDOMNode).to.be.an.elem({
+                expect(this.control).to.have.tagName('input');
+                expect(this.control).to.have.attr('type', 'radio');
+                expect(this.component).to.be.a.block('radio');
+                expect(this.control).to.be.an.elem({
                     block: 'radio',
                     elem: 'control'
                 });
             });
 
-            it('hover', function() {
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.have.mods({ hovered: true });
-
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.not.have.mods({ hovered: true });
-            });
-
-            it('focus/blur', function() {
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.have.mods({ focused: true });
-
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.not.have.mods({ focused: true });
-            });
-
-            it('disabled', function() {
-                this.renderWithProps({ disabled: true });
-                expect(this.rootComponentDOMNode).to.have.mods({ disabled: true });
-            });
-
-            it('radio group', function() {
-                const radio1 = renderOnce(Radio({ name: 'group1' }));
-                const radio1DOMNode = ReactDOM.findDOMNode(radio1);
-                const radio1InputControlDOMNode = TestUtils.findRenderedDOMComponentWithClass(radio1, 'radio__control');
-
-                const radio2 = renderOnce(Radio({ name: 'group1', checked: true }));
-                const radio2DOMNode = ReactDOM.findDOMNode(radio2);
-
-                expect(radio1DOMNode).to.not.has.mods({ checked: true });
-                expect(radio2DOMNode).to.has.mods({ checked: true });
-
-                TestUtils.Simulate.change(radio1InputControlDOMNode, { target: { checked: true } });
-
-                expect(radio1DOMNode).to.has.mods({ checked: true });
-                expect(radio2DOMNode).to.not.has.mods({ checked: true });
-            });
-
             it('children', function() {
-                this.renderWithProps({
-                    children: React.createElement('div', {
+                this.component.setProps({
+                    children: createElement('div', {
                         key: 'test',
                         className: 'test-children'
                     })
                 });
 
-                expect(
-                    TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'test-children')
-                ).to.be.block('test-children');
-            });
-        });
-
-        describe('callbacks', () => {
-            it('onChange', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({
-                    onChange(e) {
-                        spy(e.target.checked);
-                    }
-                });
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { checked: true } });
-                expect(spy).to.have.been.called.with(true);
-
-                this.renderWithProps();
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { cheked: true } });
-                expect(spy).to.have.been.called.with(true);
-            });
-
-            it('onFocus', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onFocus: spy });
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onBlur', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onBlur: spy });
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseEnter', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onMouseEnter: spy });
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseLeave', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onMouseLeave: spy });
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-        });
-
-        describe('componentWillReceiveProps', () => {
-            it('checked', function() {
-                const render = createRender();
-                let component = render(Radio({ checked: false }));
-
-                expect(component.state.checked).to.be.equal(false);
-
-                component = render(Radio({ checked: true }));
-
-                expect(component.state.checked).to.be.equal(true);
-
-                component = render(Radio({ checked: true }));
-
-                expect(component.state.checked).to.be.equal(true);
-            });
-        });
-
-        describe('componentWillUnmount', () => {
-            it('remove radio from existing group', function() {
-                const radio1 = renderOnce(Radio({ name: 'group2' }));
-                const radio1DOMNode = ReactDOM.findDOMNode(radio1);
-                const radio1InputControlDOMNode = TestUtils.findRenderedDOMComponentWithClass(radio1, 'radio__control');
-
-                const radio2 = renderOnce(Radio({ name: 'group2', checked: true }));
-                const radio2DOMNode = ReactDOM.findDOMNode(radio2);
-
-                ReactDOM.unmountComponentAtNode(radio2DOMNode.parentNode);
-                TestUtils.Simulate.change(radio1InputControlDOMNode, { target: { checked: true } });
-
-                expect(radio1DOMNode).to.has.mods({ checked: true });
-                expect(radio2DOMNode).to.has.mods({ checked: true });
+                expect(this.component.find('.test-children')).to.be.block('test-children');
             });
         });
     });

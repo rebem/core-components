980616442f5ad45f1dc3e2c56f891210c41db8ec
deepsweet <kir@soulshine.in>
2016-01-19 12:29:36 +0700

    :wrench: Attach as stateless function

--- src/attach/index.js
+++ src/attach/index.js
@@ -1,154 +1,33 @@
-import { Component, PropTypes } from 'react';
+import { PropTypes } from 'react';
 import { BEM } from 'rebem';
 
 const block = 'attach';
 
-export default class extends Component {
-    static displayName = `core: ${block}`;
-    static propTypes = {
-        value: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number
-        ]),
-        disabled: PropTypes.bool,
-        onChange: PropTypes.func,
-        onFocus: PropTypes.func,
-        onBlur: PropTypes.func,
-        onMouseEnter: PropTypes.func,
-        onMouseLeave: PropTypes.func,
-        onMouseDown: PropTypes.func,
-        onMouseUp: PropTypes.func
-    };
-    static defaultProps = {
-        disabled: false
-    };
-
-    constructor(props, context) {
-        super(props, context);
-
-        this.state = {
-            pressed: false,
-            focused: false,
-            hovered: false,
-            value: null
-        };
-
-        this._onInputChange = this._onInputChange.bind(this);
-        this._onInputFocus = this._onInputFocus.bind(this);
-        this._onInputBlur = this._onInputBlur.bind(this);
-        this._onInputMouseDown = this._onInputMouseDown.bind(this);
-        this._onInputMouseUp = this._onInputMouseUp.bind(this);
-        this._onInputMouseLeave = this._onInputMouseLeave.bind(this);
-        this._onInputMouseEnter = this._onInputMouseEnter.bind(this);
-    }
-
-    // file input doesn't allow to change it's value programmatically
-    // componentWillReceiveProps() {}
-
-    _onInputChange(e) {
-        this.setState({
-            value: e.target.value
-        });
-
-        if (typeof this.props.onChange === 'function') {
-            this.props.onChange(e);
-        }
-    }
-
-    _onInputFocus(e) {
-        this.setState({
-            focused: true
-        });
-
-        if (typeof this.props.onFocus === 'function') {
-            this.props.onFocus(e);
-        }
-    }
-
-    _onInputBlur(e) {
-        this.setState({
-            focused: false
-        });
-
-        if (typeof this.props.onBlur === 'function') {
-            this.props.onBlur(e);
-        }
-    }
-
-    _onInputMouseDown(e) {
-        this.setState({
-            pressed: true
-        });
-
-        if (typeof this.props.onMouseDown === 'function') {
-            this.props.onMouseDown(e);
-        }
-    }
-
-    _onInputMouseUp(e) {
-        this.setState({
-            pressed: false
-        });
-
-        if (typeof this.props.onMouseUp === 'function') {
-            this.props.onMouseUp(e);
-        }
-    }
-
-    _onInputMouseEnter(e) {
-        this.setState({
-            hovered: true
-        });
-
-        if (typeof this.props.onMouseEnter === 'function') {
-            this.props.onMouseEnter(e);
-        }
-    }
-
-    _onInputMouseLeave(e) {
-        this.setState({
-            hovered: false,
-            pressed: false
-        });
-
-        if (typeof this.props.onMouseLeave === 'function') {
-            this.props.onMouseLeave(e);
-        }
-    }
-
-    val() {
-        return this.state.value;
-    }
-
-    render() {
-        return BEM(
-            {
-                block,
-                tag: 'label',
-                mods: {
-                    focused: this.state.focused,
-                    hovered: this.state.hovered,
-                    pressed: this.state.pressed,
-                    disabled: this.props.disabled,
-                    ...this.props.mods
-                }
-            },
-            BEM({
-                ...this.props,
-                block,
-                elem: 'control',
-                tag: 'input',
-                type: 'file',
-                value: this.state.value,
-                onChange: this._onInputChange,
-                onFocus: this._onInputFocus,
-                onBlur: this._onInputBlur,
-                onMouseDown: this._onInputMouseDown,
-                onMouseUp: this._onInputMouseUp,
-                onMouseLeave: this._onInputMouseLeave,
-                onMouseEnter: this._onInputMouseEnter
-            }),
-            this.props.children
-        );
-    }
+export default function Attach(props) {
+    return BEM(
+        {
+            block,
+            tag: 'label',
+            mods: {
+                disabled: props.disabled,
+                ...props.mods
+            }
+        },
+        BEM({
+            ...props,
+            block,
+            elem: 'control',
+            tag: 'input',
+            type: 'file'
+        }),
+        props.children
+    );
 }
+
+Attach.defaultProps = {
+    disabled: false
+};
+
+Attach.propTypes = {
+    disabled: PropTypes.bool
+};
--- test/src/attach/index.js
+++ test/src/attach/index.js
@@ -33,30 +33,6 @@ describe('attach', function() {
                 });
             });
 
-            it('hover', function() {
-                this.control.simulate('mouseEnter');
-                expect(this.component).to.have.mods({ hovered: true });
-
-                this.control.simulate('mouseLeave');
-                expect(this.component).to.not.have.mods({ hovered: true });
-            });
-
-            it('focus/blur', function() {
-                this.control.simulate('focus');
-                expect(this.component).to.have.mods({ focused: true });
-
-                this.control.simulate('blur');
-                expect(this.component).to.not.have.mods({ focused: true });
-            });
-
-            it('pressed', function() {
-                this.control.simulate('mouseDown');
-                expect(this.component).to.have.mods({ pressed: true });
-
-                this.control.simulate('mouseUp');
-                expect(this.component).to.not.have.mods({ pressed: true });
-            });
-
             it('disabled', function() {
                 this.component.setProps({ disabled: true });
                 expect(this.component).to.have.mods({ disabled: true });
@@ -71,103 +47,6 @@ describe('attach', function() {
                 });
 
                 expect(this.component.find('.test-children')).to.be.block('test-children');
-            });
-        });
-
-        describe('API', function() {
-            it('val()', function() {
-                expect(this.component.instance().val()).to.be.equal(null);
-            });
-        });
-
-        describe('callbacks', function() {
-            it('onChange', function() {
-                const spy = chai.spy();
-
-                this.component.setProps({
-                    onChange(e) {
-                        spy(e.target.value);
-                    }
-                });
-                this.control.simulate('change', { target: { value: '' } });
-
-                this.component.setProps({ onChange: null });
-                this.control.simulate('change', { target: { value: '' } });
-
-                expect(spy).to.have.been.called.once;
-                expect(spy).to.have.been.called.with('');
-            });
-
-            it('onFocus', function() {
-                const spy = chai.spy();
-
-                this.component.setProps({ onFocus: spy });
-                this.control.simulate('focus');
-
-                this.component.setProps({ onFocus: null });
-                this.control.simulate('focus');
-
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onBlur', function() {
-                const spy = chai.spy();
-
-                this.component.setProps({ onBlur: spy });
-                this.control.simulate('blur');
-
-                this.component.setProps({ onBlur: null });
-                this.control.simulate('blur');
-
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseEnter', function() {
-                const spy = chai.spy();
-
-                this.component.setProps({ onMouseEnter: spy });
-                this.control.simulate('mouseEnter');
-
-                this.component.setProps({ onMouseEnter: null });
-                this.control.simulate('mouseEnter');
-
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseLeave', function() {
-                const spy = chai.spy();
-
-                this.component.setProps({ onMouseLeave: spy });
-                this.control.simulate('mouseLeave');
-
-                this.component.setProps({ onMouseLeave: null });
-                this.control.simulate('mouseLeave');
-
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseDown', function() {
-                const spy = chai.spy();
-
-                this.component.setProps({ onMouseDown: spy });
-                this.control.simulate('mouseDown');
-
-                this.component.setProps({ onMouseDown: null });
-                this.control.simulate('mouseDown');
-
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseUp', function() {
-                const spy = chai.spy();
-
-                this.component.setProps({ onMouseUp: spy });
-                this.control.simulate('mouseUp');
-
-                this.component.setProps({ onMouseUp: null });
-                this.control.simulate('mouseUp');
-
-                expect(spy).to.have.been.called.once;
             });
         });
     });

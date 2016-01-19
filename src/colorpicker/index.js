40e7e99a1f1359c35ccaac29c6cd78435e617ef4
deepsweet <kir@soulshine.in>
2016-01-19 12:30:24 +0700

    :wrench: Colorpicker as stateless function

--- src/colorpicker/index.js
+++ src/colorpicker/index.js
@@ -1,132 +1,33 @@
-import { Component, PropTypes } from 'react';
+import { PropTypes } from 'react';
 import { BEM } from 'rebem';
 
 const block = 'colorpicker';
 
-export default class extends Component {
-    static displayName = 'core: colorpicker';
-    static propTypes = {
-        value: (props, propName, componentName) => {
-            if (!/^#([A-Fa-f0-9]{6})$/.test(props[propName])) {
-                return new Error(`Invalid prop \`${propName}\` of type \`${typeof props[propName]}\` supplied to \`${componentName}\`, expected \`valid simple color\` (http://www.w3.org/TR/html5/infrastructure.html#valid-simple-color)`);
+export default function Colorpicker(props) {
+    return BEM(
+        {
+            block,
+            tag: 'label',
+            mods: {
+                disabled: props.disabled,
+                ...props.mods
             }
         },
-        disabled: PropTypes.bool,
-        onChange: PropTypes.func,
-        onFocus: PropTypes.func,
-        onBlur: PropTypes.func,
-        onMouseEnter: PropTypes.func,
-        onMouseLeave: PropTypes.func
-    };
-    static defaultProps = {
-        value: '#000000',
-        disabled: false
-    };
-
-    constructor(props, context) {
-        super(props, context);
-
-        this.state = {
-            value: props.value,
-            focused: false,
-            hovered: false
-        };
-
-        this._onInputChange = this._onInputChange.bind(this);
-        this._onInputFocus = this._onInputFocus.bind(this);
-        this._onInputBlur = this._onInputBlur.bind(this);
-        this._onInputMouseLeave = this._onInputMouseLeave.bind(this);
-        this._onInputMouseEnter = this._onInputMouseEnter.bind(this);
-    }
-
-    componentWillReceiveProps({ value }) {
-        if (this.props.value !== value) {
-            this.setState({
-                value
-            });
-        }
-    }
-
-    _onInputChange(e) {
-        this.setState({
-            value: e.target.value
-        });
-
-        if (this.props.onChange) {
-            this.props.onChange(e);
-        }
-    }
-
-    _onInputFocus(e) {
-        this.setState({
-            focused: true
-        });
-
-        if (this.props.onFocus) {
-            this.props.onFocus(e);
-        }
-    }
-
-    _onInputBlur(e) {
-        this.setState({
-            focused: false
-        });
-
-        if (this.props.onBlur) {
-            this.props.onBlur(e);
-        }
-    }
-
-    _onInputMouseEnter(e) {
-        this.setState({
-            hovered: true
-        });
-
-        if (this.props.onMouseEnter) {
-            this.props.onMouseEnter(e);
-        }
-    }
-
-    _onInputMouseLeave(e) {
-        this.setState({
-            hovered: false
-        });
-
-        if (this.props.onMouseLeave) {
-            this.props.onMouseLeave(e);
-        }
-    }
-
-    val() {
-        return this.state.value;
-    }
-
-    render() {
-        return BEM(
-            {
-                block,
-                tag: 'label',
-                mods: {
-                    focused: this.state.focused,
-                    hovered: this.state.hovered,
-                    disabled: this.props.disabled,
-                    ...this.props.mods
-                }
-            },
-            BEM({
-                ...this.props,
-                block,
-                elem: 'control',
-                tag: 'input',
-                type: 'color',
-                value: this.state.value,
-                onChange: this._onInputChange,
-                onFocus: this._onInputFocus,
-                onBlur: this._onInputBlur,
-                onMouseLeave: this._onInputMouseLeave,
-                onMouseEnter: this._onInputMouseEnter
-            }),
-            this.props.children
-        );
-    }
+        BEM({
+            ...props,
+            block,
+            elem: 'control',
+            tag: 'input',
+            type: 'color'
+        }),
+        props.children
+    );
 }
+
+Colorpicker.defaultProps = {
+    disabled: false
+};
+
+Colorpicker.propTypes = {
+    disabled: PropTypes.bool
+};
--- test/src/colorpicker/index.js
+++ test/src/colorpicker/index.js
@@ -1,181 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createElement } from 'react';
 import TestUtils from 'react-addons-test-utils';
 import chai, { expect } from 'chai';
-import { createRender, renderOnce } from 'test/helpers/render';
+import { shallow } from 'enzyme';
 
 import Colorpicker from '#colorpicker';
 
-describe.skip('colorpicker', () => {
-    describe('basic', () => {
-        it('exists', () => {
+describe('colorpicker', function() {
+    describe('basic', function() {
+        it('exists', function() {
             expect(Colorpicker).to.exist;
         });
 
-        it('is a component', () => {
-            expect(TestUtils.isCompositeComponent(renderOnce(Colorpicker()))).to.be.true;
+        it('is a component', function() {
+            expect(TestUtils.isElement(Colorpicker())).to.be.true;
         });
     });
 
-    describe('render', () => {
+    describe('render', function() {
         beforeEach(function() {
-            this.renderWithProps = props => {
-                this.rootComponent = renderOnce(Colorpicker(props));
-                this.rootComponentDOMNode = ReactDOM.findDOMNode(this.rootComponent);
-                this.inputControlDOMNode = TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'colorpicker__control');
-            };
-
-            this.renderWithProps();
+            this.component = shallow(Colorpicker());
+            this.control = this.component.find('.colorpicker__control');
         });
 
-        describe('DOM', () => {
+        describe('DOM', function() {
             it('initial', function() {
-                expect(this.inputControlDOMNode.tagName).to.be.equal('INPUT');
-                expect(this.inputControlDOMNode.type).to.be.equal('color');
-                expect(this.rootComponentDOMNode).to.be.a.block('colorpicker');
-                expect(this.inputControlDOMNode).to.be.an.elem({
+                expect(this.control).to.have.tagName('input');
+                expect(this.control).to.have.attr('type', 'color');
+                expect(this.component).to.be.a.block('colorpicker');
+                expect(this.control).to.be.an.elem({
                     block: 'colorpicker',
                     elem: 'control'
                 });
             });
 
-            it('hover', function() {
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.have.mods({ hovered: true });
-
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.not.have.mods({ hovered: true });
-            });
-
-            it('focus/blur', function() {
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.have.mods({ focused: true });
-
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(this.rootComponentDOMNode).to.not.have.mods({ focused: true });
-            });
-
             it('disabled', function() {
-                this.renderWithProps({ disabled: true });
-                expect(this.rootComponentDOMNode).to.have.mods({ disabled: true });
+                this.component.setProps({ disabled: true });
+                expect(this.component).to.have.mods({ disabled: true });
             });
 
-            it('value change', function() {
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: '#ff0000' } });
-                expect(this.inputControlDOMNode.value).to.be.equal('#ff0000');
-            });
-
-            it.skip('children', function() {
-                this.renderWithProps({
-                    children: React.createElement('div', {
+            it('children', function() {
+                this.component.setProps({
+                    children: createElement('div', {
                         key: 'test',
                         className: 'test-children'
                     })
                 });
 
-                expect(
-                    TestUtils.findRenderedDOMComponentWithClass(this.rootComponent, 'test-children')
-                ).to.be.block('test-children');
-            });
-        });
-
-        describe('API', () => {
-            it('val()', function() {
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: '#ff0000' } });
-                expect(this.rootComponent.val()).to.be.equal('#ff0000');
-            });
-        });
-
-        describe('callbacks', () => {
-            it('onChange', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({
-                    onChange(e) {
-                        spy(e.target.value);
-                    }
-                });
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: '#ff0000' } });
-                expect(spy).to.have.been.called.with('#ff0000');
-
-                this.renderWithProps();
-                TestUtils.Simulate.change(this.inputControlDOMNode, { target: { value: '#ff0000' } });
-                expect(spy).to.have.been.called.with('#ff0000');
-            });
-
-            it('onFocus', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onFocus: spy });
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.focus(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onBlur', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onBlur: spy });
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.blur(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseEnter', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onMouseEnter: spy });
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.mouseEnter(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-
-            it('onMouseLeave', function() {
-                const spy = chai.spy();
-
-                this.renderWithProps({ onMouseLeave: spy });
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-
-                this.renderWithProps();
-                TestUtils.Simulate.mouseLeave(this.inputControlDOMNode);
-                expect(spy).to.have.been.called.once;
-            });
-        });
-
-        describe('componentWillReceiveProps', () => {
-            it('value', function() {
-                const render = createRender();
-                let component = render(Colorpicker({ value: '#ff0000' }));
-
-                expect(component.state.value).to.be.equal('#ff0000');
-
-                component = render(Colorpicker({ value: '#00ff00' }));
-
-                expect(component.state.value).to.be.equal('#00ff00');
-
-                component = render(Colorpicker({ value: '#00ff00' }));
-
-                expect(component.state.value).to.be.equal('#00ff00');
-            });
-        });
-
-        describe('propTypes', function() {
-            it('throws error if incorrect value format', function() {
-                const incorrectRender = () => {
-                    this.renderWithProps({ value: '#000' });
-                };
-
-                expect(incorrectRender).to.throw(Error);
+                expect(this.component.find('.test-children')).to.be.block('test-children');
             });
         });
     });

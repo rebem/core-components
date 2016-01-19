506f4c6135d994a1d94958e70458342bf761833f
deepsweet <kir@soulshine.in>
2016-01-19 14:59:37 +0700

    :space_invader: fix Img tests

--- test/src/img/index.js
+++ test/src/img/index.js
@@ -1,42 +1,27 @@
 import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
-import { render } from 'test/helpers/render';
+import { shallow } from 'enzyme';
 
 import Img from '#img';
 
-describe.skip('img', () => {
-    describe('basic', () => {
-        it('exists', () => {
+describe('img', function() {
+    describe('basic', function() {
+        it('exists', function() {
             expect(Img).to.exist;
         });
 
-        it('is a component', () => {
-            render(Img)
-                .test(({ component }) => {
-                    expect(TestUtils.isCompositeComponent(component));
-                });
+        it('is a component', function() {
+            expect(TestUtils.isElement(Img())).to.be.true;
         });
     });
 
-    describe('render', () => {
-        describe('DOM', () => {
+    describe('render', function() {
+        describe('DOM', function() {
             it('initial', function() {
-                render(Img)
-                    .extractRootDOMNode()
-                    .test(({ rootDOMNode }) => {
-                        expect(rootDOMNode.tagName).to.be.equal('IMG');
-                        expect(rootDOMNode).to.be.a.block('img');
-                    });
-            });
+                const component = shallow(Img());
 
-            it('props', function() {
-                const testWidth = 200;
-
-                render(Img, { width: testWidth })
-                    .extractRootDOMNode()
-                    .test(({ rootDOMNode }) => {
-                        expect(rootDOMNode.width).to.be.equal(testWidth);
-                    });
+                expect(component.type()).to.be.equal('img');
+                expect(component).to.be.a.block('img');
             });
         });
     });

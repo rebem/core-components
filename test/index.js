84619d820d131c272071193f09047f6718f98a15
mistadikay <koltsov.d@gmail.com>
2016-01-19 17:31:17 +0700

    :wrench: use entityHook in chai-bem

--- test/index.js
+++ test/index.js
@@ -2,30 +2,19 @@ import chai from 'chai';
 import chaiBEM from 'chai-bem';
 import chaiSpies from 'chai-spies';
 import chaiEnzyme from 'chai-enzyme';
-import { ShallowWrapper } from 'enzyme';
+import { ReactWrapper, ShallowWrapper } from 'enzyme';
 
-chai.use(chaiBEM());
+chai.use(chaiBEM({
+    entityHook(entity) {
+        if (entity instanceof ShallowWrapper || entity instanceof ReactWrapper) {
+            return entity.prop('className');
+        }
+
+        return entity;
+    }
+}));
 chai.use(chaiSpies);
 chai.use(chaiEnzyme());
-chai.use((_chai, utils) => {
-    const Assertion = _chai.Assertion;
-
-    function overwrite(name) {
-        Assertion.overwriteMethod(name, (_super) => {
-            return function wrapAssertion() {
-                const entity = this._obj;
-
-                if (entity instanceof ShallowWrapper) {
-                    this._obj = entity.prop('className');
-                }
-
-                _super.apply(this, arguments);
-            };
-        });
-    }
-
-    [ 'block', 'elem', 'mods' ].forEach(overwrite);
-});
 
 const componentsContext = require.context('../src/', true, /index\.js$/);
 const testsContext = require.context('./src/', true, /index\.js$/);

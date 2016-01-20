f98bedb92a04e5ef1793cc1145624a31d02a5015
deepsweet <kir@soulshine.in>
2016-01-20 12:15:28 +0700

    :wrench: Popup as stateless function

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,111 +1,60 @@
-import { Component, PropTypes } from 'react';
+import { PropTypes } from 'react';
 import { BEM } from 'rebem';
-import UID from 'component-uid';
 
-const block = 'popup';
 const ESC_KEYCODE = 27;
-const UID_LENGTH = 20;
+const block = 'popup';
 
-export default class extends Component {
-    static displayName = `core: ${block}`;
-    static propTypes = {
-        onShow: PropTypes.func,
-        onHide: PropTypes.func
-    };
-
-    constructor(props, context) {
-        super(props, context);
-
-        this.state = {
-            visibility: false
-        };
-
-        this._onChange = this._onChange.bind(this);
-        this._onKeyUp = this._onKeyUp.bind(this);
-    }
-
-    _onKeyUp(e) {
-        if (e.keyCode === ESC_KEYCODE) {
-            this.hide();
-        }
-    }
-
-    _onChange(e) {
-        if (!e.target.checked) {
-            this.hide();
-        }
-    }
-
-    show() {
-        this.refs.popup.focus();
-
-        this.setState({
-            visibility: true
-        });
-
-        if (this.props.onShow) {
-            this.props.onShow();
-        }
-    }
-
-    hide() {
-        this.setState({
-            visibility: false
-        });
-
-        if (this.props.onHide) {
-            this.props.onHide();
-        }
-    }
-
-    render() {
-        const popupID = 'popup-' + UID(UID_LENGTH);
-
-        return BEM(
-            {
-                ...this.props,
-                block,
-                mods: {
-                    visible: this.state.visibility
-                },
-                tabIndex: -1,
-                onKeyUp: this._onKeyUp,
-                ref: 'popup'
+export default function Popup(props) {
+    return BEM(
+        {
+            ...props,
+            block,
+            mods: {
+                visible: props.visible,
+                ...props.mods
             },
-            BEM({
+            tabIndex: -1,
+            ref(ref) {
+                if (props.hideWithEsc && ref) {
+                    if (props.visible) {
+                        ref.focus();
+                    } else {
+                        ref.blur();
+                    }
+                }
+            },
+            onKeyUp(e) {
+                if (props.hideWithEsc && e.keyCode === ESC_KEYCODE) {
+                    props.onHide();
+                }
+
+                if (props.onKeyUp) {
+                    props.onKeyUp(e);
+                }
+            }
+        },
+        BEM({
+            block,
+            elem: 'overlay'
+        }),
+        BEM(
+            {
                 block,
-                elem: 'switcher',
-                tag: 'input',
-                type: 'checkbox',
-                id: popupID,
-                checked: this.state.visibility,
-                onChange: this._onChange
-            }),
-            BEM(
-                {
-                    block,
-                    elem: 'wrapper'
-                },
-                BEM({
-                    block,
-                    elem: 'overlay',
-                    tag: 'label',
-                    htmlFor: popupID
-                }),
-                BEM(
-                    {
-                        block,
-                        elem: 'inner'
-                    },
-                    BEM(
-                        {
-                            block,
-                            elem: 'content'
-                        },
-                        this.props.children
-                    )
-                )
-            )
-        );
-    }
+                elem: 'content'
+            },
+            props.children
+        )
+    );
 }
+
+Popup.propTypes = {
+    visible: PropTypes.bool,
+    hideWithEsc: PropTypes.bool,
+    onHide: PropTypes.func.isRequired,
+    onKeyUp: PropTypes.func
+};
+
+Popup.defaultProps = {
+    visible: false,
+    hideWithEsc: true
+};

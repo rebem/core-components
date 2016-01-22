84c7bee56cd46b73143b1913c6e5107889c9693a
deepsweet <kir@soulshine.in>
2016-01-22 13:19:00 +0700

    :wrench: enable `cacheDirectory` for babel-loader in demo config

--- tasks/demo.js
+++ tasks/demo.js
@@ -54,7 +54,7 @@ export function demo() {
                     ],
                     loader: 'babel',
                     query: {
-                        cacheDirectory: false
+                        cacheDirectory: true
                     }
                 }
             ],

c359a383682035c3db4114941571c67d01425d2b
mistadikay <koltsov.d@gmail.com>
2016-01-29 13:22:36 +0700

    :heavy_check_mark: fix inability to redefine type prop in input

--- src/input/index.js
+++ src/input/index.js
@@ -5,10 +5,10 @@ const block = blockFactory('input');
 export default function Input({ mods, mix, children, ...props }) {
     return block({ mods, mix, tag: 'label' },
         block({
+            type: 'text',       // type should be redefinable
             ...props,
             elem: 'control',
-            tag: 'input',
-            type: 'text'
+            tag: 'input'
         }),
         children
     );
